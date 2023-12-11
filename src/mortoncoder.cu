3c3af793d99c494a47753163d7ddc177a57768d9
Himanshu Sharma <sharmh15@li03c04.chimera.hpc.mssm.edu>
2023-12-10 22:26:01 -0500

    Added logic to compute 64 bit morton code

--- src/mortoncoder.cu
+++ src/mortoncoder.cu
@@ -9,7 +9,9 @@
 #include "util.h"
 #include <cuda_runtime_api.h>
 #include <cuda.h>
+#include <cmath>
 #include "mortoncoder.h"
+#include <bitset>
 
 using namespace cub;
 //---------------------------------------------------------------------
@@ -76,21 +78,92 @@ void Initialize(
 }
 
 */
+/*
 
+https://stackoverflow.com/a/17401122
+__device__ 
+static float atomicMax(float* address, float val)
+{
+  int* address_as_i = (int*) address;
+  int old = *address_as_i, assumed;
+  do {
+    assumed = old;
+    old = ::atomicCAS(address_as_i, assumed,
+		      __float_as_int(::fmaxf(val, __int_as_float(assumed))));
+  } while (assumed != old);
+  return __int_as_float(old);
+}
 
+__device__ 
+static float atomicMin(float* address, float val)
+{
+  int* address_as_i = (int*) address;
+  int old = *address_as_i, assumed;
+  do {
+    assumed = old;
+    old = ::atomicCAS(address_as_i, assumed,
+		      __float_as_int(::fminf(val, __int_as_float(assumed))));
+  } while (assumed != old);
+  return __int_as_float(old);
+}
+*/
 
+// Refer following for morton encoding.
+//https://www.forceflow.be/2013/10/07/morton-encodingdecoding-through-bit-interleaving-implementations/
+// https://github.com/Forceflow/libmorton  - This is a good library for CPUs
+__device__ 
+void splitBy3(int a, uint64_t& s) {
+    s = a & 0x1fffff;
+    s = (s | s << 32) & 0x1f00000000ffff;
+    s = (s | s << 16) & 0x1f0000ff0000ff;
+    s = (s | s << 8) & 0x100f00f00f00f00f;
+    s = (s | s << 4) & 0x10c30c30c30c30c3;
+    s = (s | s << 2) & 0x1249249249249249;
+}
 
 
 
 
 __global__
-void computeTriangleCentroids(float* d_vertices, int* d_indices, float* d_centroids, int nFaces) 
+void compute64BitMortonCode3dPoint(float* centroids, int count, int* bboxint, uint64_t* mortonCodes)
+{
+  //printf("here1\n");
+  int tid = blockIdx.x*blockDim.x + threadIdx.x;
+  int nt = blockDim.x*gridDim.x;
+  
+  for (int i = tid; i < count; i = i + nt) {
+
+    uint64_t s_x;
+    uint64_t s_y;
+    uint64_t s_z;
+
+    int cx_int = __float2int_rn(max((centroids[3*i] - bboxint[0])*4194303/(bboxint[3] - bboxint[0]), 0.0));
+    cx_int = cx_int <= 4194303 ? cx_int : 4194303;   // This is just to make sure there is no numerical error 
+    int cy_int = __float2int_rn(max((centroids[3*i+1] - bboxint[1])*4194303/(bboxint[4] - bboxint[1]), 0.0));
+    cy_int = cy_int <= 4194303 ? cy_int : 4194303;
+    int cz_int = __float2int_rn(max((centroids[3*i+2] - bboxint[2])*4194303/(bboxint[5] - bboxint[2]), 0.0));
+    cz_int = cz_int <= 4194303 ? cz_int : 4194303;
+    
+    splitBy3(cx_int, s_x);
+    splitBy3(cy_int, s_y);
+    splitBy3(cz_int, s_z);
+    mortonCodes[i] = s_x | s_y << 1 | s_z << 2 ;
+    
+  }
+  
+  
+}
+
+
+
+__global__
+void computeTriangleCentroids(float* d_vertices, int* d_indices, float* d_centroids, int nFaces, int* bbox) 
 {
   
   int tid = blockIdx.x*blockDim.x + threadIdx.x;
-
+  int nt = blockDim.x*gridDim.x;
   
-  for(int i = tid; i < nFaces; i = i+nFaces) { 
+  for(int i = tid; i < nFaces; i = i+nt) { 
     int idx1 = d_indices[3*i + 0];
     int idx2 = d_indices[3*i + 1];
     int idx3 = d_indices[3*i + 2];
@@ -106,11 +179,24 @@ void computeTriangleCentroids(float* d_vertices, int* d_indices, float* d_centro
     float x3 = d_vertices[3*idx3 + 0];
     float y3 = d_vertices[3*idx3 + 1];
     float z3 = d_vertices[3*idx3 + 2];
+    
+    float c_x = (x1 + x2 + x3)/3.0;
+    float c_y = (y1 + y2 + y3)/3.0;
+    float c_z = (z1 + z2 + z3)/3.0;
 
-    d_centroids[3*i+0] = (x1 + x2 + x3)/3.0;
-    d_centroids[3*i+1] = (y1 + y2 + y3)/3.0;
-    d_centroids[3*i+2] = (z1 + z2 + z3)/3.0;
+    d_centroids[3*i+0] = c_x;
+    d_centroids[3*i+1] = c_y;
+    d_centroids[3*i+2] = c_z;
 
+    
+    atomicMin(&bbox[0], __float2int_rd(c_x));
+    atomicMin(&bbox[1], __float2int_rd(c_y)); 
+    atomicMin(&bbox[2], __float2int_rd(c_z)); 
+
+    atomicMax(&bbox[3], __float2int_ru(c_x));
+    atomicMax(&bbox[4], __float2int_ru(c_y)); 
+    atomicMax(&bbox[5], __float2int_ru(c_z));
+    
   }
  
 }
@@ -126,18 +212,15 @@ void copyTinyObjSceneToGPU(tinyobj::attrib_t& attrib, std::vector<tinyobj::shape
   // For now maybe just copy vertices and face indices for only first shape ?
   // They will fire me for writing this code.
 
-
   error = cudaMalloc(&d_vertices, attrib.vertices.size()*sizeof(float));
   if(error) return;
   error = cudaMemcpy(d_vertices, attrib.vertices.data(), attrib.vertices.size()*sizeof(float), cudaMemcpyHostToDevice);
   if(error) return;
 
-
   int n = shapes[0].mesh.indices.size();
   assert (n%3 == 0);
   nFaces = n/3;
   std::vector<int> vertex_indices(n);
-  std::cout << "nFaces: " << nFaces << "\n";
   
   std::transform(shapes[0].mesh.indices.cbegin(), shapes[0].mesh.indices.cend(), vertex_indices.cbegin(),
 		 vertex_indices.begin(), [](tinyobj::index_t idx, int i) -> int { return idx.vertex_index; });
@@ -146,7 +229,6 @@ void copyTinyObjSceneToGPU(tinyobj::attrib_t& attrib, std::vector<tinyobj::shape
   if(error) return;
   error = cudaMemcpy(d_indices, vertex_indices.data(), n*sizeof(int), cudaMemcpyHostToDevice);
   if(error) return;
-  
   
   return;
 }
@@ -161,8 +243,8 @@ cudaError_t cuda_kernel(tinyobj::attrib_t attrib, std::vector<tinyobj::shape_t> 
   int* d_indices;
   int nFaces = -1;
   cudaError_t error = cudaSuccess;
-  
 
+  
   error = cudaMalloc(&d_vertices, attrib.vertices.size()*sizeof(float));
   if(error) return error;
   error = cudaMemcpy(d_vertices, attrib.vertices.data(), attrib.vertices.size()*sizeof(float), cudaMemcpyHostToDevice);
@@ -181,21 +263,86 @@ cudaError_t cuda_kernel(tinyobj::attrib_t attrib, std::vector<tinyobj::shape_t> 
   std::cout << "Suggested block Size: " << blockSize << "\n";
   std::cout << "Suggested mingrid Size: " << minGridSize << "\n";
 
-
   float* d_centroids;
   error = cudaMalloc(&d_centroids, 3*nFaces*sizeof(float));
+  if(error) {
+    std::cout << cudaGetErrorString(error);
+    std::cout <<  "exit from here\n";
+    return error;
+  }
+
+  int* bbox;
+  error = cudaMalloc(&bbox, 6*sizeof(int));
+  if(error) {
+    std::cout <<  "exit from here22\n";
+    return error;
+  }
+  int inftyup = std::numeric_limits<int>::max();
+  int inftydown = std::numeric_limits<int>::min();
+  int* bboxstart = new int[6];
+  bboxstart[0] = inftyup;
+  bboxstart[1] = inftyup;
+  bboxstart[2] = inftyup;
+  bboxstart[3] = inftydown;
+  bboxstart[4] = inftydown;
+  bboxstart[5] = inftydown;
+
+  error = cudaMemcpy(bbox, bboxstart, 6*sizeof(int), cudaMemcpyHostToDevice);
+
+  if(error) {
+    std::cout <<  "exit from here2\n";
+    return error;
+  }
+  dim3 dimGrid(minGridSize,1,1);
+  dim3 dimBlock(blockSize,1,1);
+
+  cudaDeviceSynchronize();
+
+  error = cudaGetLastError();
+  if(error) {
+    std::cout << cudaGetErrorString(error) << " before first\n";
+    return error;
+  }
+  std::cout << "centroid nfaces " << nFaces << "\n";
+  computeTriangleCentroids<<<32, 64>>>(d_vertices, d_indices, d_centroids, nFaces, bbox);
+  cudaDeviceSynchronize();
+
+  error = cudaGetLastError();
+  if(error) {
+    std::cout << cudaGetErrorString(error) << " first\n";
+    return error;
+  }
+
+  int* hostbbox = (int*)malloc(6*sizeof(int));
+  cudaMemcpy(hostbbox, bbox, 6*sizeof(int), cudaMemcpyDeviceToHost);
+
+  std::cout << "bbox : " << hostbbox[0] << ","<< hostbbox[1] << ","<< hostbbox[2] << ","<< hostbbox[3] << ","<< hostbbox[4] << ","<< hostbbox[5] << "\n";
+
+  uint64_t* mortonCodes;
+  error = cudaMalloc(&mortonCodes, nFaces*sizeof(uint64_t));
   if(error) return error;
 
-  dim3 dimGrid(32,1,1);
-  dim3 dimBlock(128,1,1);
+  error = cudaOccupancyMaxPotentialBlockSize(&minGridSize, &blockSize, compute64BitMortonCode3dPoint, 0, nFaces); 
+  if(error) return error;
+  dim3 dimGrid2(32,1,1);
+  dim3 dimBlock2(32,1,1);
 
   cudaDeviceSynchronize();
-  computeTriangleCentroids<<<dimGrid, dimBlock>>>(d_vertices, d_indices, d_centroids, nFaces);
+  compute64BitMortonCode3dPoint<<<dimGrid2, dimBlock2>>>(d_centroids, nFaces, bbox, mortonCodes);
   cudaDeviceSynchronize();
 
+  error = cudaGetLastError();
+  if(error) {
+    std::cout << cudaGetErrorString(error) << " over here\n";
+    return error;
+  }
+  
   
   float* centroids = (float*)malloc(3*nFaces*sizeof(float));
   cudaMemcpy(centroids, d_centroids, 3*nFaces*sizeof(float), cudaMemcpyDeviceToHost);
+  uint64_t* mortonhost = (uint64_t*)malloc(nFaces*sizeof(uint64_t));
+  cudaMemcpy(mortonhost, mortonCodes, nFaces*sizeof(uint64_t), cudaMemcpyDeviceToHost);
+
   cudaDeviceSynchronize();
   for(int i = 0; i < 100; i++) {
     int idx1 = shapes[0].mesh.indices[i].vertex_index;
@@ -216,7 +363,7 @@ cudaError_t cuda_kernel(tinyobj::attrib_t attrib, std::vector<tinyobj::shape_t> 
 
 
     std::cout << x1 << " " << x2 << " " << x3 << y1 << " " << y2 << " " << y3 << z1 << " " << z2 << " " << z3 << "\n";
-    std::cout << centroids[i]  << "\n";
+    std::cout << centroids[3*i]  << " " << centroids[3*i + 1]  << " " << centroids[3*i+2] << " " << std::bitset<64>(mortonhost[i]) << "\n";
   }
   
 
